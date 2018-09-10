8f77666dc6b6cbdc8aaaa2b1a9c6526a0485415c
Chris Smith <chris@cgsmith.net>
2018-09-10 10:56:12 -0500

    Calendar Updates

--- frontend/src/website/events/components/EventCalendar.js
+++ frontend/src/website/events/components/EventCalendar.js
@@ -9,10 +9,14 @@ export default function EventCalendar() {
 
       <h1 className="center" style={{ fontSize: '2.4em' }}>Nature&apos;s Classroom Montessori School Calendar</h1>
 
-      {/* <Row>
+        <p className="center">
+            <a href="/documents/calendar.pdf" className="btn btn-success"><span aria-hidden="true" className="fa fa-file-pdf-o fa-fw"></span>Calendar Download</a>
+        </p>
+      <Row>
         <Col md={8} mdOffset={2}>
 
           <iframe
+            title="Nature's Classroom Montessori Google Calendar"
             src="//www.google.com/calendar/embed?showTitle=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=650&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=5srg9q0f9tt99a4eb9ehi3c0po%40group.calendar.google.com&amp;color=%23B1440E&amp;ctz=America%2FChicago"
             width="100%"
             height="550"
@@ -27,25 +31,26 @@ export default function EventCalendar() {
 
         </Col>
       </Row>
-
       <p className="center">
         <a
           href="https://calendar.google.com/calendar/ical/5srg9q0f9tt99a4eb9ehi3c0po%40group.calendar.google.com/public/basic.ics"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="nofollow noopener noreferrer"
         >
           Subscribe to the Google Calendar
         </a>{' '}
-        version of our Calendar so that NCI events show up along-side all your other calendars.
-      </p> */}
+        version of our Calendar so that NCM events show up along-side all your other calendars.
+      </p>
 
       <Row className="top-30">
         <Col md={10} mdOffset={1} xs={12}>
           <img
             src="/discovernci_media/calendar.jpg"
             className="img-responsive"
-            alt=""
+            alt="Montessori Calendar"
             style={{ marginBottom: 60 }}
+            height="1200"
+            width="1566"
           />
         </Col>
       </Row>
