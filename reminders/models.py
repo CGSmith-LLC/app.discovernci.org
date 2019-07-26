51c22a2ddbe59ef111a7c09dceca1c26f28f7d1a
Ben Keating <ben@abstractly.io>
2019-07-25 23:01:01 -0500

    NOTE: run manage.py migrate! Reminder.html field now using CKEditor

--- reminders/models.py
+++ reminders/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 # from django.template.defaultfilters import slugify
 
 # from jsonfield import JSONField
@@ -10,7 +11,7 @@ from .managers import ReminderManager
 class Reminder(models.Model):
     fieldtrip = models.ForeignKey('locations.FieldTrip', blank=False, null=False)
     subject = models.CharField(max_length=128, blank=False)
-    html = models.TextField(blank=False)
+    html = RichTextUploadingField(blank=False)
     send_date = models.DateField()
     sent = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
