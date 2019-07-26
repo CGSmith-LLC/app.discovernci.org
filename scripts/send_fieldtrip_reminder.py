39871ab93748ff0899b4290fd69a453b019b1db8
Ben Keating <ben@abstractly.io>
2019-07-26 14:43:36 -0500

    Comments

--- scripts/send_fieldtrip_reminder.py
+++ scripts/send_fieldtrip_reminder.py
@@ -18,6 +18,7 @@ from reminders.models import Reminder
 def run():
     from_email = "Nature's Classroom <no-reply@discovernci.org>"
 
+    # Grab all Reminders marked for today *and* earlier (just in case)
     todays_reminders = Reminder.objects.filter(
         send_date__lte=datetime.today().date(),
         fieldtrip__is_enabled=True,
@@ -27,12 +28,17 @@ def run():
 
     for reminder in todays_reminders:
 
+        # Send to all [is_active] teachers belonging to any of the schools
+        # affiliated with this field trip.
         send_to_list = reminder.fieldtrip.get_teacher_email_list()
+
+        # Reply back to this field trip locations primary contact
         reply_to_list = ["{} <{}>".format(
             reminder.fieldtrip.location.primary_contact.name,
             reminder.fieldtrip.location.primary_contact.email
         )]
 
+        # Custom signature based on location's primary contact details
         signature = """\
             {}<br />\n
             {}<br />\n
@@ -53,6 +59,7 @@ def run():
         html_body = reminder.html + "\n" + signature
         plain_text_body = strip_tags(html_body)
 
+        # Create and send out the Email
         msg = EmailMultiAlternatives(
             reminder.subject,
             plain_text_body,
@@ -63,5 +70,6 @@ def run():
         msg.attach_alternative(html_body, "text/html")  # html-formatted bodyx
         msg.send()
 
+        # Mark sent reminders as being sent
         reminder.sent = True
         reminder.save()
