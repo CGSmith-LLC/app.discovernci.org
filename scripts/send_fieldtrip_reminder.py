a83dbcbb69f6c24f9384e848740443410d94a8b4
Ben Keating <ben@abstractly.io>
2019-07-26 14:32:06 -0500

    `send_fieldtrip_reminder` now constructs signature, dynamic [teacher] to_list, reply-to ee staff

--- scripts/send_fieldtrip_reminder.py
+++ scripts/send_fieldtrip_reminder.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-Send a Membership Renewal Email to our Members Billing Representatives.
+Send a reminder email to Teachers about their upcoming FieldTrips.
 
-USAGE: $ ./manage.py runscript send_renewal_invoice
+USAGE: $ ./manage.py runscript send_fieldtrip_reminder
 
-Running this script will query the database for all Active Members in our
-system and email them an html/plain-text email on how to renew their membership.
 """
 
 from datetime import datetime
@@ -18,41 +16,52 @@ from reminders.models import Reminder
 
 
 def run():
+    from_email = "Nature's Classroom <no-reply@discovernci.org>"
+
     todays_reminders = Reminder.objects.filter(
         send_date__lte=datetime.today().date(),
         fieldtrip__is_enabled=True,
         is_active=True,
         sent=False
     )
-    for reminder in todays_reminders:
-        # Convert plain-text body to string to concatenate variables into the body
-        # text_content = """\
-        #     You have an upcoming fieldtrip:
-        #     """ + str(reminder.fieldtrip) + """ on """ + str(reminder.fieldtrip.start_date) + """
-        #     Please contact us if you have any questions.
-        # """
 
-        # Convert html body to string to concatenate variables into the body
-        # html_content = """\
-        # <html>
-        #   <head></head>
-        #   <body>
-        #     <p>You have an upcoming fieldtrip:<br>
-        #        <br>
-        #        """ + str(reminder.fieldtrip) + """ on """ + str(reminder.fieldtrip.start_date) + """
-        #     </p>
-        #     additional info: """ + str(reminder.html) + """
-        #   </body>
-        # </html>
-        # """
+    for reminder in todays_reminders:
+
+        signature = """\
+            {}<br />\n
+            {}<br />\n
+            Nature’s Classroom Institute and Montessori School<br />\n
+            Direct Phone: {}<br />\n
+            Office Phone: {}<br />\n
+            www.discovernci.org<br />\n
+            Like us on Facebook: www.facebook.com/NaturesClassroomInstitute<br />\n
+            Follow us on Instagram: www.instagram.com/NaturesClassroomInstitute<br />\n
+            <img src="https://app.discovernci.org/email-signature-logo.png" alt="" />
+        """.format(
+            reminder.fieldtrip.location.primary_contact.name,
+            reminder.fieldtrip.location.primary_contact.title,
+            reminder.fieldtrip.location.primary_contact.phone,
+            reminder.fieldtrip.location.phone
+        )
+
+        html_body = reminder.html + "\n" + signature
+        plain_text_body = strip_tags(html_body)
+
+        send_to_list = reminder.fieldtrip.get_teacher_email_list()
+
+        reply_to_list = ["{} <{}>".format(
+            reminder.fieldtrip.location.primary_contact.name,
+            reminder.fieldtrip.location.primary_contact.email
+        )]
 
         msg = EmailMultiAlternatives(
-            reminder.subject,  # subject
-            strip_tags(reminder.html),  # plain-text body
-            'no-reply@discovernci.org',  # from-email
-            [i.email for i in reminder.reminder_addresses.all()]  # to email list
+            reminder.subject,
+            plain_text_body,
+            from_email,
+            send_to_list,
+            reply_to=reply_to_list
         )
-        msg.attach_alternative(reminder.html, "text/html")  # html-formatted bodyx
+        msg.attach_alternative(html_body, "text/html")  # html-formatted bodyx
         msg.send()
 
         reminder.sent = True
