da9b884b96f62f42655d50afd8a79d1c1b412581
Chris Smith <chris@cgsmith.net>
2019-07-30 23:47:48 -0500

    - removing unnecessary comments

--- reminders/models.py
+++ reminders/models.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
-# from django.template.defaultfilters import slugify
-
-# from jsonfield import JSONField
 
 from .managers import ReminderManager
 
